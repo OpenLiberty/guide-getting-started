5be3d9ca23cfc7ad1f13c6eb9ecc0f16b2336355
Justine Chen <justine.chenibm.com@justines-mbp.war.can.ibm.com>
2019-03-21 15:04:14 -0400

    Updated SystemHealth.java and server.xml with mpConfig feature to check inmaintenance instead of server name

--- finish/src/main/java/io/openliberty/sample/system/SystemHealth.java
+++ finish/src/main/java/io/openliberty/sample/system/SystemHealth.java
@@ -14,6 +14,10 @@ package io.openliberty.sample.system;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import javax.inject.Inject;
+import javax.inject.Provider;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
@@ -22,12 +26,16 @@ import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 @Health
 @ApplicationScoped
 public class SystemHealth implements HealthCheck {
+
+    @Inject
+    @ConfigProperty(name = "io_openliberty_guides_system_inMaintenance")
+    Provider<String> inMaintenance;
 	
     @Override
     public HealthCheckResponse call() {
         HealthCheckResponseBuilder builder = HealthCheckResponse.named(
 		SystemResource.class.getSimpleName());
-        if (!System.getProperty("wlp.server.name").equals("GettingStartedServer")) {
+        if (inMaintenance != null && inMaintenance.get().equalsIgnoreCase("true")) {
             return builder.withData("services", "not available").down().build();
         }
         return builder.withData("services", "available").up().build();

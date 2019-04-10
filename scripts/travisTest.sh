a5369d742ae6da64e89e0b850be06d0f6867e420
Andrew <andrew.deschenes@ibm.com>
2019-04-10 19:07:21 -0400

    Push to artifactory in travis.yaml instead

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -39,17 +39,3 @@ echo -e "\n"
 echo -e  "\033[1;34mOpen Liberty release:\033[0m \033[1;36m$release\033[0m"
 echo -e "\033[1;34mOpen Liberty build number:\033[0m \033[1;36m$number\033[0m" 
 echo -e "\033[1;34mJava version:\033[0m\033[1;36m$jv\033[0m"
-
-cd target/liberty/wlp/usr/servers/"$serverName"/logs/; 
-repo_name=$(echo "$TRAVIS_REPO_SLUG" | sed -e "s/\//-/g");
- 
-if [ $TRAVIS_TEST_RESULT -eq 0 ] 
-then 
-  result="passed"
-else 
-  result="failed" 
-fi
- 
-serverlogsarchive="$repo_name-$TRAVIS_BUILD_NUMBER-$result.zip"
-zip -r "$serverlogsarchive" .
-curl -H "$JFROG_TOKEN" -T "$serverlogsarchive" "https://na.artifactory.swg-devops.com/artifactory/hyc-openliberty-guides-files-generic-local/"

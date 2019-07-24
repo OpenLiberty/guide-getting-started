5d34d144404a7337e902c919f98ebee88f2ff80e
Andrew <andrew.deschenes@ibm.com>
2019-07-24 15:14:03 -0400

    Addressing Gilberts feedback

--- finish/src/main/java/io/openliberty/sample/system/SystemLivenessCheck.java
+++ finish/src/main/java/io/openliberty/sample/system/SystemLivenessCheck.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2018, 2019  IBM Corporation and others.
+ * Copyright (c) 2019  IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,7 +20,6 @@ import java.lang.management.ManagementFactory;
 import org.eclipse.microprofile.health.Liveness;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
-import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 
 @Liveness
 @ApplicationScoped
@@ -32,7 +31,8 @@ public class SystemLivenessCheck implements HealthCheck {
         long memUsed = memBean.getHeapMemoryUsage().getUsed();
         long memMax = memBean.getHeapMemoryUsage().getMax();
   
-        return HealthCheckResponse.named(SystemResource.class.getSimpleName() + " liveness check")
+        return HealthCheckResponse.named(
+            SystemResource.class.getSimpleName() + " liveness check")
                                   .withData("memory used", memUsed)
                                   .withData("memory max", memMax)
                                   .state(memUsed < memMax * 0.9).build();

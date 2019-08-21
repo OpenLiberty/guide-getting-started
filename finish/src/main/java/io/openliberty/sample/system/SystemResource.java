f3ce77ea9fcf76704cb3c1ac093528568bae0e45
Gilbert Kwan <gkwan@ca.ibm.com>
2019-08-21 11:17:15 -0400

    Update SystemResource.java

--- finish/src/main/java/io/openliberty/sample/system/SystemResource.java
+++ finish/src/main/java/io/openliberty/sample/system/SystemResource.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017, 2018 IBM Corporation and others.
+ * Copyright (c) 2017, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,7 +30,7 @@ public class SystemResource {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Timed(name = "getPropertiesTime", description = "Time needed to get the JVM system properties")
-	@Counted(absolute = true, monotonic = true, description = "Number of times the JVM system properties are requested")
+	@Counted(absolute = true, description = "Number of times the JVM system properties are requested")
 	public Response getProperties() {
 	    return Response.ok(System.getProperties()).build();
 	}

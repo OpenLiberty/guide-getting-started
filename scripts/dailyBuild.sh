132b8021dc0f683f0ee77e70ba6a4d7c90a3fb98
Austin Seto <austin.seto@ibm.com>
2020-11-23 15:11:16 -0500

    Updates to daily build script

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DEVDATE"/"$DRIVER"</runtimeUrl></install></configuration>" pom.xml
+cat pom.xml
+
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILDLEVEL";g" Dockerfile
+cat Dockerfile
+
 set -euxo pipefail
 
 ##############################################################################

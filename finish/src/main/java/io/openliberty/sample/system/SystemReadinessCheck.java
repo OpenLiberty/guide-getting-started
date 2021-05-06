8d3ee487aa9f292d67dbe30f740516b2d4f47e1e
Gilbert Kwan <gkwan@ca.ibm.com>
2021-05-06 16:14:23 -0400

    Update SystemReadinessCheck.java

--- finish/src/main/java/io/openliberty/sample/system/SystemReadinessCheck.java
+++ finish/src/main/java/io/openliberty/sample/system/SystemReadinessCheck.java
@@ -27,13 +27,13 @@ import org.eclipse.microprofile.health.HealthCheckResponse;
 // tag::systemReadinessCheck[]
 public class SystemReadinessCheck implements HealthCheck {
 
-    private static final String readinessCheck = SystemResource.class.getSimpleName() 
+    private static final String readinessCheck = SystemResource.class.getSimpleName()
                                                  + " Readiness Check";
 
     @Inject
     @ConfigProperty(name = "io_openliberty_guides_system_inMaintenance")
     Provider<String> inMaintenance;
-	
+
     @Override
     public HealthCheckResponse call() {
         if (inMaintenance != null && inMaintenance.get().equalsIgnoreCase("true")) {
@@ -41,6 +41,6 @@ public class SystemReadinessCheck implements HealthCheck {
         }
         return HealthCheckResponse.up(readinessCheck);
     }
-    
-}
+
+}    
 // end::systemReadinessCheck[]

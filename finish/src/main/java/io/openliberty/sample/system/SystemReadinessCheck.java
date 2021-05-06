a47a97ea775449b5b3d0b7f1e59f07c316e85edb
Gilbert Kwan <gkwan@ca.ibm.com>
2021-05-06 16:22:24 -0400

    Update SystemReadinessCheck.java

--- finish/src/main/java/io/openliberty/sample/system/SystemReadinessCheck.java
+++ finish/src/main/java/io/openliberty/sample/system/SystemReadinessCheck.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2018, 2021  IBM Corporation and others.
+ * Copyright (c) 2018, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -42,5 +42,5 @@ public class SystemReadinessCheck implements HealthCheck {
         return HealthCheckResponse.up(READINESS_CHECK);
     }
 
-}    
+}
 // end::systemReadinessCheck[]

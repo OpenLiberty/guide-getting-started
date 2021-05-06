a30472ba20965454819e8c1f7b7ca90ee063c990
Gilbert Kwan <gkwan@ca.ibm.com>
2021-05-06 15:22:11 -0400

    Update SystemLivenessCheck.java

--- finish/src/main/java/io/openliberty/sample/system/SystemLivenessCheck.java
+++ finish/src/main/java/io/openliberty/sample/system/SystemLivenessCheck.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2019, 2020 IBM Corporation and others.
+ * Copyright (c) 2019, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -36,7 +36,7 @@ public class SystemLivenessCheck implements HealthCheck {
             SystemResource.class.getSimpleName() + " Liveness Check")
                                   .withData("memory used", memUsed)
                                   .withData("memory max", memMax)
-                                  .state(memUsed < memMax * 0.9).build();
+                                  .status(memUsed < memMax * 0.9).build();
     }
     
 }

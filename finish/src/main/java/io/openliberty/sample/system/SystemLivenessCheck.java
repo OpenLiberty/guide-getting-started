3b7b11c101ecac6349d79bd4f85b315da64d1dda
Gilbert Kwan <gkwan@ca.ibm.com>
2021-05-06 16:12:51 -0400

    Update SystemLivenessCheck.java

--- finish/src/main/java/io/openliberty/sample/system/SystemLivenessCheck.java
+++ finish/src/main/java/io/openliberty/sample/system/SystemLivenessCheck.java
@@ -31,13 +31,13 @@ public class SystemLivenessCheck implements HealthCheck {
         MemoryMXBean memBean = ManagementFactory.getMemoryMXBean();
         long memUsed = memBean.getHeapMemoryUsage().getUsed();
         long memMax = memBean.getHeapMemoryUsage().getMax();
-  
+
         return HealthCheckResponse.named(
             SystemResource.class.getSimpleName() + " Liveness Check")
                                   .withData("memory used", memUsed)
                                   .withData("memory max", memMax)
                                   .status(memUsed < memMax * 0.9).build();
     }
-    
+
 }
 // end::systemLivenessCheck[]

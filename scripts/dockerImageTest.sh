0cdc5b30380dff078aa091a06750398baca81993
Gilbert Kwan <gkwan@ca.ibm.com>
2023-07-10 14:45:01 -0400

    Update dockerImageTest.sh
    full image does not support features.sh

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -14,6 +14,7 @@ sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install
 cat pom.xml
 
 sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM openliberty/daily:latest;g" Dockerfile
+sed -i "s;RUN features.sh;#RUN features.sh;g" Dockerfile
 cat Dockerfile
 
 docker pull "openliberty/daily:latest"

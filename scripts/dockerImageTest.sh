ab2c70060222ca4b0837428f15e5928d7bed8a0f
Gilbert Kwan <gkwan@ca.ibm.com>
2023-11-24 14:31:51 -0500

    Update dockerImageTest.sh

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -17,6 +17,9 @@ sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp
 sed -i "s;RUN features.sh;#RUN features.sh;g" Dockerfile
 cat Dockerfile
 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin cp.stg.icr.io
 docker pull "cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi"
+echo "build level:"
+docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi
 
 ../scripts/testApp.sh

39f72839edbf9ab0b2c28acc104c75d42214fa6a
Gilbert Kwan <gkwan@ca.ibm.com>
2025-01-31 16:58:12 -0500

    Use versionless features (#422)
    * use versionless feature
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>
    
    * use versionless feature
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>
    
    * use versionless feature
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>
    
    * use versionless feature
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>
    
    * use versionless feature
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>
    
    * use versionless feature
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>
    
    * Update mpData.js
    
    ---------
    
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>

--- start/src/main/webapp/js/mpData.js
+++ start/src/main/webapp/js/mpData.js
@@ -1,11 +1,10 @@
 /*******************************************************************************
-* Copyright (c) 2018, 2019 IBM Corporation and others.
+* Copyright (c) 2018, 2025 IBM Corporation and others.
 * All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
+* are made available under the terms of the Eclipse Public License 2.0
 * which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
+* http://www.eclipse.org/legal/epl-2.0/
 *
-* Contributors:
 *     IBM Corporation - initial API and implementation
 *******************************************************************************/
 function displayMetrics() {
@@ -85,7 +84,8 @@ function displaySystemProperties() {
 }
 
 function getSystemPropertiesRequest() {
-    var propToDisplay = ["java.vendor", "java.version", "user.name", "os.name", "wlp.install.dir", "wlp.server.name" ];
+    var propToDisplay = ["java.vendor", "java.version", "user.name",
+                         "os.name", "wlp.install.dir", "wlp.server.name" ];
     var url = "http://localhost:9080/system/properties";
     var req = new XMLHttpRequest();
     var table = document.getElementById("systemPropertiesTable");
